fc0b908e67580b63226f36bb7d594b83fc2b5237
Tony David <70279376+handleryouth@users.noreply.github.com>
2021-05-08 13:35:03 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const leftArrow = document.querySelector(".left-selection")
 const rightArrow = document.querySelector(".right-selection")
 
-var first = document.querySelector(".slide1")
-var second = document.querySelector(".slide2")
-var third = document.querySelector(".slide3")
+var first = document.querySelector(".image1-description")
+var second = document.querySelector(".image2-description")
+var third = document.querySelector(".image3-description")
 
+var image1 = document.querySelector(".image1")
+var image2 = document.querySelector(".image2")
+var image3 = document.querySelector(".image3")
 
 var tracker = 1
 
@@ -13,60 +16,60 @@ rightArrow.addEventListener("click", function(){
   tracker += 1
   if (tracker < 1){
     first.classList.remove("notshowed")
-    first.classList.add("flex-display")
+    image1.classList.remove("notshowed")
 
     second.classList.add("notshowed")
-    second.classList.remove("flex-display")
+    image2.classList.add("notshowed")
 
     third.classList.add("notshowed")
-    third.classList.remove("flex-display")
+    image3.classList.add("notshowed")
     tracker = 3
   }
 
   if (tracker === 1){
     first.classList.remove("notshowed")
-    first.classList.add("flex-display")
+    image1.classList.remove("notshowed")
 
     second.classList.add("notshowed")
-    second.classList.remove("flex-display")
+    image2.classList.add("notshowed")
 
     third.classList.add("notshowed")
-    third.classList.remove("flex-display")
+    image3.classList.add("notshowed")
   }
 
   if (tracker === 2){
     first.classList.add("notshowed")
-    first.classList.remove("flex-display")
+    image1.classList.add("notshowed")
 
     second.classList.remove("notshowed")
-    second.classList.add("flex-display")
+    image2.classList.remove("notshowed")
 
     third.classList.add("notshowed")
-    third.classList.remove("flex-display")
+    image3.classList.add("notshowed")
 
   }
 
   if (tracker === 3){
     first.classList.add("notshowed")
-    first.classList.remove("flex-display")
+    image1.classList.add("notshowed")
 
     second.classList.add("notshowed")
-    second.classList.remove("flex-display")
+    image2.classList.add("notshowed")
 
     third.classList.remove("notshowed")
-    third.classList.add("flex-display")
+    image3.classList.remove("notshowed")
     tracker = 0
   }
 
   if (tracker > 3){
     first.classList.remove("notshowed")
-    first.classList.add("flex-display")
+    image1.classList.remove("notshowed")
 
     second.classList.add("notshowed")
-    second.classList.remove("flex-display")
+    image2.classList.add("notshowed")
 
     third.classList.add("notshowed")
-    third.classList.remove("flex-display")
+    image3.classList.add("notshowed")
     tracker = 1
   }
 })
@@ -75,50 +78,50 @@ leftArrow.addEventListener("click", function(){
   tracker -= 1
   if (tracker < 0){
     first.classList.add("notshowed")
-    first.classList.remove("flex-display")
+    image1.classList.add("notshowed")
 
     second.classList.remove("notshowed")
-    second.classList.add("flex-display")
+    image2.classList.remove("notshowed")
 
     third.classList.add("notshowed")
-    third.classList.remove("flex-display")
+    image3.classList.add("notshowed")
     tracker = 2
   }
 
 
   if (tracker === 0){
     first.classList.add("notshowed")
-    first.classList.remove("flex-display")
+    image1.classList.add("notshowed")
 
     second.classList.add("notshowed")
-    second.classList.remove("flex-display")
+    image2.classList.add("notshowed")
 
     third.classList.remove("notshowed")
-    third.classList.add("flex-display")
+    image3.classList.remove("notshowed")
     tracker = 3
   }
 
   if (tracker === 1){
     first.classList.remove("notshowed")
-    first.classList.add("flex-display")
+    image1.classList.remove("notshowed")
 
     second.classList.add("notshowed")
-    second.classList.remove("flex-display")
+    image2.classList.add("notshowed")
 
     third.classList.add("notshowed")
-    third.classList.remove("flex-display")
+    image3.classList.add("notshowed")
 
   }
 
   if (tracker === 2){
     first.classList.add("notshowed")
-    first.classList.remove("flex-display")
+    image1.classList.add("notshowed")
 
     second.classList.remove("notshowed")
-    second.classList.add("flex-display")
+    image2.classList.remove("notshowed")
 
     third.classList.add("notshowed")
-    third.classList.remove("flex-display")
+    image3.classList.add("notshowed")
   }
 
 })
