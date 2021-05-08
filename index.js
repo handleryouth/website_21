23d3bfb92c024a7c8ea18f4b1bf24d315addea5c
Tony David <70279376+handleryouth@users.noreply.github.com>
2021-05-08 14:07:35 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+const navbar = document.querySelector('.navbar')
+const hamburgerIcon = document.querySelector(".hamburgerIcon")
+const closeIcon = document.querySelector(".closeIcon")
+const roomLogo = document.querySelector(".middle-navigation-image")
+const navigationLink1 = document.querySelectorAll(".middle-navigation-link")[0]
+const navigationLink2 = document.querySelectorAll(".middle-navigation-link")[1]
+const navigationLink3 = document.querySelectorAll(".middle-navigation-link")[2]
+const navigationLink4 = document.querySelectorAll(".middle-navigation-link")[3]
+
+hamburgerIcon.addEventListener("click", function() {
+  navigationLink1.classList.remove("notshowed")
+  navigationLink2.classList.remove("notshowed")
+  navigationLink3.classList.remove("notshowed")
+  navigationLink4.classList.remove("notshowed")
+  roomLogo.classList.add("notshowed")
+  hamburgerIcon.classList.add("notshowed")
+  closeIcon.classList.remove("notshowed")
+  navbar.classList.add("white-background")
+})
+
+
+closeIcon.addEventListener("click", function(){
+  navigationLink1.classList.add("notshowed")
+  navigationLink2.classList.add("notshowed")
+  navigationLink3.classList.add("notshowed")
+  navigationLink4.classList.add("notshowed")
+  roomLogo.classList.remove("notshowed")
+  hamburgerIcon.classList.remove("notshowed")
+  closeIcon.classList.add("notshowed")
+  navbar.classList.remove("white-background")
+})
+
+
+
+
 const leftArrow = document.querySelector(".left-selection")
 const rightArrow = document.querySelector(".right-selection")
 
